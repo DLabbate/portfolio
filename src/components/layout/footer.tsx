d42baae7550fc224f92c33ba8d9aef9327802214
Domenic Labbate <domeniclabbate@gmail.com>
2023-07-17 01:46:56 -0400

    Padding

--- src/components/layout/footer.tsx
+++ src/components/layout/footer.tsx
@@ -3,7 +3,7 @@ import SocialLink from "../social-link";
 
 const Footer = () => {
   return (
-    <footer className="mt-8 flex w-full flex-col items-center justify-center gap-2 p-2 text-sm text-dark-medium">
+    <footer className="mt-8 flex w-full flex-col items-center justify-center gap-2 p-4 text-sm text-dark-medium">
       <div className="flex items-center gap-4 xl:hidden">
         {SOCIAL_MEDIA.map((item) => (
           <SocialLink key={item.platform} {...item} />
