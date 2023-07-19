7f36250c929a6ffc3cc5fed4932e8a9ad28114fd
Domenic Labbate <domeniclabbate@gmail.com>
2023-07-18 21:28:09 -0400

    Improving header

--- src/components/layout/header.tsx
+++ src/components/layout/header.tsx
@@ -5,34 +5,64 @@ import { usePathname } from "next/navigation";
 import { Menu, Moon, X } from "react-feather";
 import Link from "next/link";
 import * as motion from "@/components/animations/motion";
-import SocialLink from "../social-link";
-import { SOCIAL_MEDIA } from "@/constants/profile";
+import useMediaQuery from "@/hooks/useMediaQuery";
 
-type Props = {
+type Page = {
   title: string;
   href: string;
 };
 
-const HeaderLink = ({ title, href }: Props) => {
+const LINKS: ReadonlyArray<Page> = [
+  { title: "Home", href: "/" },
+  { title: "Portfolio", href: "/portfolio" },
+  { title: "Blogs", href: "/blogs" },
+];
+
+type Props = {
+  title: string;
+  href: string;
+  onClick: () => void;
+};
+
+const HeaderLink = ({ title, href, onClick }: Props) => {
+  const isSmallDevice = useMediaQuery("only screen and (max-width : 1024px)");
+  const pathname = usePathname();
+  const active: boolean = pathname === href;
   return (
     <motion.div
       whileHover="hover"
+      animate={active ? "active" : "inactive"}
       className="relative flex w-full cursor-pointer items-center justify-center border-y border-primary-2 p-8 text-xl lg:block lg:w-auto lg:border-0 lg:p-0 "
     >
-      <Link className="lg:h-auto lg:w-auto" href={href}>
-        {title}
-      </Link>
-      <motion.div
-        className="hidden h-1 bg-neutral-50 lg:absolute lg:block"
-        variants={{ hover: { width: "100%" } }}
-      />
+      <div
+        className="w-full lg:h-auto lg:w-auto"
+        onClick={isSmallDevice ? onClick : () => {}}
+      >
+        <Link
+          className="flex h-full w-full items-center justify-center"
+          href={href}
+        >
+          {title}
+        </Link>
+      </div>
+      {!isSmallDevice && (
+        <motion.div
+          className="absolute h-1 bg-neutral-50"
+          variants={{
+            hover: { width: "100%" },
+            active: { width: "100%" },
+            inactive: { width: 0 },
+          }}
+        />
+      )}
     </motion.div>
   );
 };
 
 const Header = () => {
   const [mobileMenuOpen, setMobileMenuOpen] = useState(false);
-  const pathname = usePathname();
+
+  const toggleMenu = () => setMobileMenuOpen(!mobileMenuOpen);
 
   useEffect(() => {
     const body = document.querySelector("body");
@@ -44,7 +74,7 @@ const Header = () => {
   return (
     <motion.header
       animate={mobileMenuOpen ? "open" : "closed"}
-      className="fixed top-0 z-50 h-auto w-full overflow-hidden bg-primary opacity-95"
+      className="fixed top-0 z-50 h-auto w-full overflow-hidden bg-primary/75 backdrop-blur-md"
       variants={{ open: { height: "100%" } }}
       transition={{ ease: "linear" }}
     >
@@ -57,24 +87,16 @@ const Header = () => {
           variants={{ open: { opacity: 1 } }}
         >
           <motion.nav className="flex flex-col items-center justify-center border-y border-primary-2 lg:flex-row lg:gap-16 lg:border-y-0">
-            <HeaderLink title="Home" href="/" />
-            <HeaderLink title="Portfolio" href="/portfolio" />
-            <HeaderLink title="Blogs" href="/blogs" />
-          </motion.nav>
-          <div className="m-8 flex items-center justify-center gap-8 lg:hidden">
-            {SOCIAL_MEDIA.map((item) => (
-              <SocialLink key={item.platform} {...item} />
+            {LINKS.map((link) => (
+              <HeaderLink key={link.title} {...link} onClick={toggleMenu} />
             ))}
-          </div>
+          </motion.nav>
         </motion.div>
         <div className="flex flex-1 justify-end gap-4 lg:flex">
           <button>
             <Moon strokeWidth={1} size={30} />
           </button>
-          <button
-            className="lg:hidden"
-            onClick={() => setMobileMenuOpen(!mobileMenuOpen)}
-          >
+          <button className="lg:hidden" onClick={toggleMenu}>
             {mobileMenuOpen ? (
               <X strokeWidth={1} size={30} />
             ) : (
