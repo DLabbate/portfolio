da3d093f30b223902dd997607feffc9620beb95d
Domenic Labbate <domeniclabbate@gmail.com>
2023-08-29 17:58:53 -0400

    Added animations to portfolio page

--- src/components/animations/motion.tsx
+++ src/components/animations/motion.tsx
@@ -8,9 +8,11 @@
 import { motion } from "framer-motion";
 
 export const div = motion.div;
+export const h1 = motion.h1;
 export const a = motion.a;
 export const span = motion.span;
 export const li = motion.li;
 export const ul = motion.ul;
 export const header = motion.header;
 export const nav = motion.nav;
+export const section = motion.section;
--- src/components/layout/section.tsx
+++ src/components/layout/section.tsx
@@ -1,14 +1,30 @@
+import * as motion from "../animations/motion";
+
 type Props = {
   title: string;
   children?: React.ReactNode;
 };
 
+const VARIANTS = {
+  hidden: { opacity: 0, y: 20 },
+  visible: { opacity: 1, y: 0 },
+} as const;
+
 const Section = ({ title, children }: Props) => {
   return (
-    <section className="flex w-full flex-col items-center">
-      <h1 className="mb-8 text-3xl font-bold">{title}</h1>
-      {children}
-    </section>
+    <motion.section
+      className="flex w-full flex-col items-center"
+      initial={"hidden"}
+      whileInView={"visible"}
+      variants={VARIANTS}
+      transition={{ staggerChildren: 0.25 }}
+      viewport={{ once: true }}
+    >
+      <motion.h1 className="mb-8 text-3xl font-bold" variants={VARIANTS}>
+        {title}
+      </motion.h1>
+      <motion.div variants={VARIANTS}>{children}</motion.div>
+    </motion.section>
   );
 };
 
