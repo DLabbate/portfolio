a8db89cda5e7f2f51d17a7376e4c58544a06983f
Domenic Labbate <domeniclabbate@gmail.com>
2023-09-13 00:24:00 -0400

    Added search icon

--- src/components/forms/input.tsx
+++ src/components/forms/input.tsx
@@ -1,13 +1,27 @@
+import { Search } from "react-feather";
+
 type Props = {
   placeholder: string;
 };
 
 const Input = ({ placeholder }: Props) => {
   return (
-    <input
-      className="w-full rounded-lg border border-primary-200 p-2 ring-inset focus:outline-0 focus:ring-2 focus:ring-primary-100 dark:border-primary-800 dark:bg-primary-900 dark:focus:ring-primary-800"
-      placeholder={placeholder}
-    />
+    <div className="relative w-full rounded-lg border border-primary-200 dark:border-primary-800">
+      <div className="pointer-events-none absolute inset-y-0 left-0 flex items-center pl-4">
+        <Search
+          strokeWidth={1}
+          size={24}
+          className="stroke-light-medium dark:stroke-dark-medium"
+        />
+      </div>
+      <input
+        type="text"
+        name="search"
+        id="search"
+        className="block w-full rounded-lg border-0 bg-white py-1.5 pl-14 ring-inset placeholder:text-light-medium focus:outline-0 focus:ring-2 focus:ring-inset focus:ring-primary-100 dark:bg-primary-900 dark:placeholder:text-dark-medium dark:focus:ring-primary-800"
+        placeholder="Search..."
+      />
+    </div>
   );
 };
 
