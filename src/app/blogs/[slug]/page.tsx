96dcfa074268ceb7f199d165bcf01730da4575d8
Domenic Labbate <domeniclabbate@gmail.com>
2023-09-18 18:21:34 -0400

    Playing more with the styling

--- src/app/blogs/[slug]/page.tsx
+++ src/app/blogs/[slug]/page.tsx
@@ -1,5 +1,6 @@
 import { Mdx } from "@/components/mdx";
 import { allBlogs } from "contentlayer/generated";
+import Image from "next/image";
 import { notFound } from "next/navigation";
 
 const findBlogBySlug = (slug: string) =>
@@ -21,9 +22,28 @@ const BlogPost = ({ params }: { params: { slug: string } }) => {
   if (!blog) return notFound();
 
   return (
-    <div>
-      <div className="mb-8 flex flex-col items-center justify-center gap-2">
-        <h1 className="text-4xl font-bold">{blog.title}</h1>
+    <div className="w-full max-w-5xl items-center gap-8">
+      <h1 className="text-4xl font-bold">{blog.title}</h1>
+      {/* <Image
+        priority
+        src={blog.imageSrc}
+        width={800}
+        height={400}
+        className="rounded-lg"
+        alt={blog.title}
+      /> */}
+
+      <div className="relative aspect-[3/2] w-full overflow-hidden rounded-lg">
+        <Image priority src={blog.imageSrc} fill alt={blog.title} />
+      </div>
+
+      <div className="flex flex-col gap-2 self-start">
+        <span className="text-light-medium dark:text-dark-medium">
+          Published: April 17, 1998
+        </span>
+        <span className="text-light-medium dark:text-dark-medium">
+          Last Edited: April 17, 1998
+        </span>
       </div>
       <Mdx code={blog.body.code} />
     </div>
