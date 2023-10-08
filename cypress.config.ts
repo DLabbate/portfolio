5d4a148cb63cabdc4b45e281382b1ae8ae32ea34
Domenic Labbate <domeniclabbate@gmail.com>
2023-10-08 17:07:25 -0400

    Cypress tests

--- cypress.config.ts
+++ cypress.config.ts
@@ -1,6 +1,8 @@
 import { defineConfig } from "cypress";
 
 export default defineConfig({
+  // https://github.com/cypress-io/cypress/issues/3623
+  scrollBehavior: false,
   e2e: {
     setupNodeEvents(on, config) {
       // implement node event listeners here
--- cypress/e2e/blogs.cy.ts
+++ cypress/e2e/blogs.cy.ts
@@ -1,4 +1,4 @@
-describe("blogs page", () => {
+describe("blogs list", () => {
   beforeEach(() => {
     cy.visit("/blogs");
   });
@@ -7,34 +7,72 @@ describe("blogs page", () => {
     cy.getAllLinks();
   });
 
-  it.only("checks search input works", () => {
-    cy.scrollTo("top");
-
+  it("checks input fields work", () => {
     cy.get('[placeholder="Search..."]').should("be.visible");
     cy.contains("Filter By Tag").should("be.visible");
     cy.contains("Sort By").should("be.visible");
 
-    // Tags
-    cy.contains("Microservices")
-      .should("have.attr", "aria-pressed", "false")
-      .click({ scrollBehavior: false });
-
-    cy.contains("Microservices").should("have.attr", "aria-pressed", "true");
-
     // Search Input
-    cy.getBySel("blog-search-input").type("Scalab", { scrollBehavior: false });
+    const searchQuery = "Scalab";
+    cy.getBySel("blog-search-input").type(searchQuery);
+    cy.location().should((loc) => {
+      expect(loc.search).to.include(`search=${searchQuery}`);
+    });
+
+    // Tags
+    const tag = "Microservices";
+    cy.contains(tag)
+      .should("have.attr", "aria-pressed", "false")
+      .click()
+      .should("have.attr", "aria-pressed", "true");
+    cy.location().should((loc) => {
+      expect(loc.search).to.include(`tags=${tag}`);
+    });
 
     // Sort By
-    cy.getBySel("blog-sortby")
-      .should("be.visible")
-      .click({ scrollBehavior: false });
+    cy.getBySel("blog-sortby").should("be.visible").click();
+    cy.contains("Date (Oldest to Newest)").should("be.visible").click();
+    cy.location().should((loc) => {
+      expect(loc.search).to.include("sortBy=date-asc");
+    });
 
-    cy.contains("Date (Oldest to Newest)")
-      .should("be.visible")
-      .click({ scrollBehavior: false });
-
+    // Expected Blog
+    cy.location().should((loc) => {
+      expect(loc.search).to.eq(
+        "?search=Scalab&tags=Microservices&sortBy=date-asc"
+      );
+    });
     cy.contains("Scalability With Asynchronous Messaging")
       .scrollIntoView()
-      .should("be.visible");
+      .should("be.visible")
+      .click();
+    cy.location().should((loc) => {
+      expect(loc.pathname).to.eq("/blogs/async-messaging");
+    });
+  });
+
+  it("checks tags can be toggled", () => {
+    const tag1 = "Microservices";
+    const tag2 = "Design Patterns";
+
+    cy.contains(tag1)
+      .should("have.attr", "aria-pressed", "false")
+      .click()
+      .should("have.attr", "aria-pressed", "true");
+
+    cy.contains(tag2)
+      .should("have.attr", "aria-pressed", "false")
+      .click()
+      .should("have.attr", "aria-pressed", "true");
+
+    cy.location().should((loc) => {
+      expect(loc.search).to.eq("?tags=Microservices&tags=Design+Patterns");
+    });
+
+    cy.contains(tag2).click().should("have.attr", "aria-pressed", "false");
+
+    cy.location().should((loc) => {
+      expect(loc.search).to.eq("?tags=Microservices");
+    });
   });
 });
