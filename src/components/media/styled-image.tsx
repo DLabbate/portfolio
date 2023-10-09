7471645db00494bd64f6c466dcf9d926ff0d0702
Domenic Labbate <domeniclabbate@gmail.com>
2023-10-08 22:38:59 -0400

    image sizes

--- src/components/media/styled-image.tsx
+++ src/components/media/styled-image.tsx
@@ -7,6 +7,7 @@ const StyledImage = ({ alt, src, className, ...rest }: ImageProps) => {
       className={clsx("rounded-md", className)}
       alt={alt}
       src={src}
+      sizes="(max-width: 1024px) 100vw, 1024px"
       quality={100}
       {...rest}
     />
